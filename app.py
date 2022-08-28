e29089d6b75156ce16db0c1d5450777a712648be
jaxphuchanlmht1999 <107495688+jaxphuchanlmht1999@users.noreply.github.com>
2022-08-28 11:44:22 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+
+@app.route('/post', methods=["POST"])
+def predict():
+    input_json = request.get_json(force=True)
+    check_key  = input_json['key']
+
+    if('TVTSQT-eF7Vj6vXe8' == check_key):
+        load_model = pickle.load(open('model.pkl','rb'))
+        load_vectorizer = pickle.load(open('save_vectorizer.pkl','rb'))
+
+        y_pred = load_model.predict(load_vectorizer.transform([input_json['text']]))
+        result = {'y_pred' : str(y_pred)}
+
+        return jsonify(result)
+    else:
+        return 'Key Error'
+
+@app.route('/')
+def main():
+    return render_template('predict.html')
+
+@app.route('/sub', methods = ['POST'])
+def submit():
+    if request.method == 'POST':
+        text = request.form['text']
+        key = request.form['key']
+
+    if('TVTSQT-eF7Vj6vXe8' == key):
+        load_model = pickle.load(open('model.pkl','rb'))
+        load_vectorizer = pickle.load(open('save_vectorizer.pkl','rb'))
+
+        y_pred = load_model.predict(load_vectorizer.transform([text]))
+
+        return render_template('sub.html', kq = str(y_pred))
+    else:
+        return render_template('sub.html', kq = 'Key Error')
+
+if __name__ == '__main__':
+    app.run(debug=True)
