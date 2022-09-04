9cbcb6fad743ca350360ff191eddce5f1a4cd1ff
jaxphuchanlmht1999 <107495688+jaxphuchanlmht1999@users.noreply.github.com>
2022-09-04 12:14:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def submit():
     if request.method == 'POST':
 #         text = request.form['text']
 #         key = request.form['key']
-        text_key = request.get_json(cache = True)
+        text_key = request.get_json()
         text = text_key['text']
         key = text_key['key']
 
